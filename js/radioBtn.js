f92c7b87724eb7621c86457ad8a1ec80d2a7110f
AM111D <Dimasssnike@icloud.com>
2023-06-22 15:58:41 -0400

    add_file_and_header

---
+++ js/radioBtn.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const radioButtons = document.querySelectorAll('input[type="radio"]');
+  const labelUA = document.querySelector('#language-ua');
+  const labelRU = document.querySelector('#language-ru');
+
+  radioButtons.forEach(function (radioButton) {
+    radioButton.addEventListener('change', function () {
+      const selectedValue = document.querySelector(
+        'input[type="radio"]:checked'
+      ).value;
+
+      if (selectedValue === 'ua') {
+        labelUA.style.color = '#FFFFFF';
+        labelRU.style.color = '#000000';
+      } else if (selectedValue === 'ru') {
+        labelUA.style.color = '#000000';
+        labelRU.style.color = '#FFFFFF';
+      }
+
+      // Сохранение выбранного языка в localStorage
+      localStorage.setItem('selectedLanguage', selectedValue);
+    });
+  });
+
+  // Проверка сохраненного языка при загрузке страницы
+  const selectedLanguage = localStorage.getItem('selectedLanguage');
+  if (selectedLanguage === 'ua') {
+    radioButtons[0].checked = true;
+    labelUA.style.color = '#FFFFFF';
+    labelRU.style.color = '#000000';
+  } else if (selectedLanguage === 'ru') {
+    radioButtons[1].checked = true;
+    labelUA.style.color = '#000000';
+    labelRU.style.color = '#FFFFFF';
+  }
+});
